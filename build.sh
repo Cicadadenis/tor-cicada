cebc8ff9f7eb7a645870357cbc6c9fd91a3ea840
Cicada 3301 <50529176+Cicadadenis@users.noreply.github.com>
2021-06-07 00:59:07 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,21 +5,21 @@ echo "Installing dependencies "
 sudo pip3 install -r requirements.txt 
 mkdir build
 cd build
-cython3 ../torghost.py --embed -o torghost.c --verbose
+cython3 ../tor-cicada.py --embed -o tor-cicada.c --verbose
 if [ $? -eq 0 ]; then
     echo [SUCCESS] Generated C code
 else
     echo [ERROR] Build failed. Unable to generate C code using cython3
     exit 1
 fi
-gcc -Os -I /usr/include/python3.8 -o torghost torghost.c -lpython3.8 -lpthread -lm -lutil -ldl
+gcc -Os -I /usr/include/python3.8 -o tor-cicada tor-cicada.c -lpython3.8 -lpthread -lm -lutil -ldl
 if [ $? -eq 0 ]; then
     echo [SUCCESS] Compiled to static binay 
 else
     echo [ERROR] Build failed
     exit 1
 fi
-sudo cp -r torghost /usr/bin/
+sudo cp -r tor-cicada /usr/bin/
 if [ $? -eq 0 ]; then
     echo [SUCCESS] Copied binary to /usr/bin 
 else
