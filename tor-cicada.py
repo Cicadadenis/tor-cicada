cf0eb73d07b754c87c475516221d50dec45e0594
Cicada 3301 <50529176+Cicadadenis@users.noreply.github.com>
2021-06-07 00:36:26 +0300

    Update tor-cicada.py

--- tor-cicada.py
+++ tor-cicada.py
@@ -31,7 +31,7 @@ class bcolors:
     BOLD = '\033[1m'
     BGRED = '\033[41m'
     WHITE = '\033[37m'
-
+    
 
 def t():
     current_time = time.localtime()
@@ -47,29 +47,19 @@ def sigint_handler(signum, frame):
 def logo():
     print(bcolors.RED + bcolors.BOLD)
     print("""
-      _____           ____ _               _
-     |_   _|__  _ __ / ___| |__   ___  ___| |_
-       | |/ _ \| '__| |  _| '_ \ / _ \/ __| __|
-       | | (_) | |  | |_| | | | | (_) \__ \ |_
-       |_|\___/|_|   \____|_| |_|\___/|___/\__|
+ _____           ____ _               _       
+|_   _|__  _ __ / ___(_) ___ __ _  __| | __ _ 
+  | |/ _ \| '__| |   | |/ __/ _` |/ _` |/ _` |
+  | | (_) | |  | |___| | (_| (_| | (_| | (_| |
+  |_|\___/|_|   \____|_|\___\__,_|\__,_|\__,_|
+
 	{V} - github.com/Cicadadenis/
 
     """.format(V=VERSION))
     print(bcolors.ENDC)
-
-
-def usage():
-    logo()
-    print("""
-    Torghost usage:
-    -s    --start       Старт Torghost
-    -r    --switch      Request new tor exit node
-    -x    --stop        Стоп Torghost
-    -h    --help        print(this help and exit)
-
-    """)
-    sys.exit()
-
+    
+    
+    
 
 def ip():
     while True:
@@ -84,7 +74,7 @@ def ip():
 
 def check_root():
     if os.geteuid() != 0:
-        print("You must be root; Say the magic word 'sudo'")
+        print("Вы должны быть root; Скажи волшебное слово 'sudo'")
         sys.exit(0)
 
 
@@ -105,70 +95,65 @@ resolvString = 'nameserver 127.0.0.1'
 Torrc = '/etc/tor/torghostrc'
 resolv = '/etc/resolv.conf'
 
+logo()
 
-def start_torghost():
-    print(t() + ' Always check for updates using -u option')
-    os.system('sudo cp /etc/resolv.conf /etc/resolv.conf.bak')
-    if os.path.exists(Torrc) and TorrcCfgString in open(Torrc).read():
-        print(t() + ' Torrc file already configured')
-    else:
+os.system('sudo cp /etc/resolv.conf /etc/resolv.conf.bak')
+if os.path.exists(Torrc) and TorrcCfgString in open(Torrc).read():
+    print(t() + ' Файл Torrc уже настроен')
+else:
 
-        with open(Torrc, 'w') as myfile:
-            print(t() + ' Writing torcc file ')
-            myfile.write(TorrcCfgString)
-            print(bcolors.GREEN + '[done]' + bcolors.ENDC)
-    if resolvString in open(resolv).read():
-        print(t() + ' DNS resolv.conf file already configured')
-    else:
-        with open(resolv, 'w') as myfile:
-            print(t() + ' Configuring DNS resolv.conf file.. '),
-            myfile.write(resolvString)
-            print(bcolors.GREEN + '[done]' + bcolors.ENDC)
+    with open(Torrc, 'w') as myfile:
+        print(t() + ' Запись файла torcc ')
+        myfile.write(TorrcCfgString)
+        print(bcolors.GREEN + '[done]' + bcolors.ENDC)
+if resolvString in open(resolv).read():
+    print(t() + 'Файл DNS resolv.conf уже настроен')
+else:
+    with open(resolv, 'w') as myfile:
+        print(t() + ' Настройка файла DNS resolv.conf.. '),
+        myfile.write(resolvString)
+        print(bcolors.GREEN + '[done]' + bcolors.ENDC)
+ 	
+ 	
+ 	
+os.system('sudo systemctl stop tor')
+os.system('sudo fuser -k 9051/tcp > /dev/null 2>&1')
+print(bcolors.GREEN + '[done]' + bcolors.ENDC)
+print(t() + ' Запуск нового демона tor '),
+os.system('sudo -u debian-tor tor -f /etc/tor/torghostrc > /dev/null'
+          )
+print(bcolors.GREEN + '[done]' + bcolors.ENDC)
+print(t() + ' настройка правил iptables'),
 
-    print(t() + ' Stopping tor service '),
-    os.system('sudo systemctl stop tor')
-    os.system('sudo fuser -k 9051/tcp > /dev/null 2>&1')
-    print(bcolors.GREEN + '[done]' + bcolors.ENDC)
-    print(t() + ' Starting new tor daemon '),
-    os.system('sudo -u debian-tor tor -f /etc/tor/torghostrc > /dev/null'
-              )
-    print(bcolors.GREEN + '[done]' + bcolors.ENDC)
-    print(t() + ' setting up iptables rules'),
+iptables_rules = \
+    """
+NON_TOR="192.168.1.0/24 192.168.0.0/24"
+TOR_UID=%s
+TRANS_PORT="9040"
+iptables -F
+iptables -t nat -F
 
-    iptables_rules = \
-        """
-	NON_TOR="192.168.1.0/24 192.168.0.0/24"
-	TOR_UID=%s
-	TRANS_PORT="9040"
-
-	iptables -F
-	iptables -t nat -F
-
-	iptables -t nat -A OUTPUT -m owner --uid-owner $TOR_UID -j RETURN
-	iptables -t nat -A OUTPUT -p udp --dport 53 -j REDIRECT --to-ports 5353
-	for NET in $NON_TOR 127.0.0.0/9 127.128.0.0/10; do
-	 iptables -t nat -A OUTPUT -d $NET -j RETURN
-	done
-	iptables -t nat -A OUTPUT -p tcp --syn -j REDIRECT --to-ports $TRANS_PORT
-
+iptables -t nat -A OUTPUT -m owner --uid-owner $TOR_UID -j RETURN
+iptables -t nat -A OUTPUT -p udp --dport 53 -j REDIRECT --to-ports 5353
+for NET in $NON_TOR 127.0.0.0/9 127.128.0.0/10; do
+ iptables -t nat -A OUTPUT -d $NET -j RETURN
+done
+iptables -t nat -A OUTPUT -p tcp --syn -j REDIRECT --to-ports $TRANS_PORT
 	iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-	for NET in $NON_TOR 127.0.0.0/8; do
-	 iptables -A OUTPUT -d $NET -j ACCEPT
-	done
-	iptables -A OUTPUT -m owner --uid-owner $TOR_UID -j ACCEPT
-	iptables -A OUTPUT -j REJECT
-	""" \
-        % subprocess.getoutput('id -ur debian-tor')
-
-    os.system(iptables_rules)
-    print(bcolors.GREEN + '[done]' + bcolors.ENDC)
-    print(t() + ' Fetching current IP...')
-    print(t() + ' CURRENT IP : ' + bcolors.GREEN + ip() + bcolors.ENDC)
-
-
+for NET in $NON_TOR 127.0.0.0/8; do
+ iptables -A OUTPUT -d $NET -j ACCEPT
+done
+iptables -A OUTPUT -m owner --uid-owner $TOR_UID -j ACCEPT
+iptables -A OUTPUT -j REJECT
+""" \
+    % subprocess.getoutput('id -ur debian-tor')
+os.system(iptables_rules)
+print(bcolors.GREEN + '[done]' + bcolors.ENDC)
+print(t() + 'Получение IP...')
+print(t() + ' ТЕКУЩИЙ IP : ' + bcolors.GREEN + ip() + bcolors.ENDC)
 def stop_torghost():
-    print(bcolors.RED + t() + 'STOPPING torghost' + bcolors.ENDC)
-    print(t() + ' Flushing iptables, resetting to default'),
+    print(bcolors.RED + t() + 'ОСТАНОВКА TorCicada' + bcolors.ENDC)
+    print(t() + ' Очистка iptables, сброс на значения по умолчанию'),
     os.system('mv /etc/resolv.conf.bak /etc/resolv.conf')
     IpFlush = \
         """
@@ -183,44 +168,9 @@ def stop_torghost():
     os.system(IpFlush)
     os.system('sudo fuser -k 9051/tcp > /dev/null 2>&1')
     print(bcolors.GREEN + '[done]' + bcolors.ENDC)
-    print(t() + ' Restarting Network manager'),
+    print(t() + ' Перезапуск сетевого менеджера'),
     os.system('service network-manager restart')
     print(bcolors.GREEN + '[done]' + bcolors.ENDC)
-    print(t() + ' Fetching current IP...')
+    print(t() + ' Получение IP...')
     time.sleep(3)
-    print(t() + ' CURRENT IP : ' + bcolors.GREEN + ip() + bcolors.ENDC)
-
-
-
-
-
-
-
-def main():
-    check_root()
-    if len(sys.argv) <= 1:
-
-        usage()
-    try:
-        (opts, args) = getopt.getopt(sys.argv[1:], 'srxhu', [
-            'start', 'stop', 'switch', 'help', 'update'])
-    except (getopt.GetoptError):
-        usage()
-        sys.exit(2)
-    for (o, a) in opts:
-        if o in ('-h', '--help'):
-            usage()
-        elif o in ('-s', '--start'):
-            start_torghost()
-        elif o in ('-x', '--stop'):
-            stop_torghost()
-        elif o in ('-r', '--switch'):
-            switch_tor()
-        elif o in ('-u', '--update'):
-            check_update()
-        else:
-            usage()
-
-
-if __name__ == '__main__':
-    main()
+    print(t() + ' ТЕКУЩИЙ IP : ' + bcolors.GREEN + ip() + bcolors.ENDC)
